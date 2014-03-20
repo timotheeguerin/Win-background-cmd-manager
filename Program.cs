be63bc612312c5a9032edce4dbca523554d1ff83
Timothee Guerin <timothee.guerin@outlook.com>
2014-03-20 13:57:23 -0400

    inital commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CmdInTray
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Show the system tray icon.          
+            using (ProcessIcon pi = new ProcessIcon())
+            {
+                pi.Display();
+
+                // Make sure the application runs!
+                Application.Run();
+            }
+        }
+    }
+}
